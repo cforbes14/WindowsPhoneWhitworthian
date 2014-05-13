d617b2ab47ced5f2613338f2fc440898e073328a
unknown <cforbes14@LAB-SCI-304-15.whitworth.edu>
2014-05-12 20:22:29 -0700

    Got scrolling text

---
+++ Whitworthian/ExpandContent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Whitworthian
+{
+    class ExpandContent
+    {
+        public ExpandContent(string content)
+        {
+            var splitText = TextBlockSplitter.Instance.Split(content, 20, FontWeights.Normal,
+                                             Application.Current.Host.Content.ActualWidth);
+            Paragraphs = new ObservableCollection<string>(splitText);
+        }
+
+        public ObservableCollection<string> Paragraphs { get; set; }
+    }
+}
